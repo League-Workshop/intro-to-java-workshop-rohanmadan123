afbc403b09071c6e582b58ace2d446b81ce4545b
root <root@6e975a9fff26>
2020-07-15 18:57:33 +0000

    Day 3

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,25 +7,32 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
+		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it.");
 		// Get the user to enter an adjective
-
+		String typeAdj = JOptionPane.showInputDialog("Enter an adjective");
 		// Get the user to enter a type of liquid
-
+		String typeLiquid = JOptionPane.showInputDialog("Enter a type of liquid");
 		// Get the user to enter a body part
-
+		String typeBodyp = JOptionPane.showInputDialog("Enter a body part");
 		// Get the user to enter a verb
-
+		String typeVerb = JOptionPane.showInputDialog("Enter a verb");
 		// Get the user to enter a place
-
+		String typePlace = JOptionPane.showInputDialog("Enter a place");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
 		// likely take a bite out of your [body part] if you [verb]. Whatever
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
+		String madlib = "Piranhas are more " + typeAdj + " during the day, so cross the river at \n"
+				+ "night. Piranhas are more attracted to fresh " + typeLiquid + " and will most \n"
+						+ "likely take a bit out of your " + typeBodyp + " if you " + typeVerb + ". Whatever \n"
+								+ "you do, if you have an open wound, try to find another way to get \n"
+								+ "back to the " + typePlace + ". Good luck!";
 		
 		// Make a pop-up for the final story. You can use \n to go to the next line
+		JOptionPane.showMessageDialog(null, madlib);
+		
 		
 
 	}
--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -7,46 +7,62 @@ import javax.swing.ImageIcon;
 public class DragonFight {
 	// 1. Put all of the game code inside the main method (below) 
 	public static void main(String[] args) {
+	
 		
 		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+		int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth = 100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int playerDamage;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
-		
+		int dragonDamage;
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+		String typeYellKick = JOptionPane.showInputDialog("Do you want to attack the dragon with a yell or a kick?");
+		String kick = "kick";
+	    String yell = "yell";
 		// 9. If they typed in "yell":
-		
+		Random randy = new Random();
+		int randNum = randy.nextInt(10 + 1);
+		if(typeYellKick.equalsIgnoreCase(yell)) {
+			dragonHealth -= randNum;
+		}
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable 
 			
 		// 10. If they typed in "kick":
-		
+		int randNum2 = randy.nextInt(25 + 1);
+		if(typeYellKick.equalsIgnoreCase(kick)) {
+			dragonHealth -= randNum2;
+		}
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable
 			
 			
-		
+		int randNum3 = randy.nextInt(35 + 1);
+		playerHealth -= randNum3; 
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
 		
 		// 12. Subtract this number from the player's health
 		
 		
-		
+		if(playerHealth <= 0) {
+			JOptionPane.showMessageDialog(null, "You lose!");
+		} else if(dragonHealth <= 0){
+			JOptionPane.showMessageDialog(null, "The dragon is dead and you have taken a ton of gold!.");
+		} else {
+			JOptionPane.showMessageDialog(null, "Your current health " + playerHealth + ". The dragons health " + dragonHealth + " .");
+		}
 		// 13. If the user's health is less than or equal to 0
 		
 			//-- Tell the user that they lost
@@ -60,6 +76,8 @@ public class DragonFight {
 			
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
 			
-		}
+		
+	}
+}
 	}
 
